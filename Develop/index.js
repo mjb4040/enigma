6d82a685a244d33721114ec902e0583826c0514d
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-10-16 10:42:01 -0400

    modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = require('inquirer');
+    function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
